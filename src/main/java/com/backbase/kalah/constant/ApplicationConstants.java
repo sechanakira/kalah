4cf7e3d8cab2c66bbfbd1e6d9a398aae7c333dca
Shingirai Chanakira <sechanakira@yahoo.com>
2018-11-25 15:51:13 +0200

    Added Application Constants

---
+++ src/main/java/com/backbase/kalah/constant/ApplicationConstants.java
@@ -0,0 +1,10 @@
+package com.backbase.kalah.constant;
+
+public class ApplicationConstants {
+
+    private ApplicationConstants(){
+    }
+
+    public static final String GAME_RESOURCE_BASE_URL = "http://localhost:8080/games/";
+
+}
