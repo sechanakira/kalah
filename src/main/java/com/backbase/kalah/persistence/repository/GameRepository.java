db83b36846e3bebb36ed46673f76d81780a5286b
Shingirai Chanakira <sechanakira@yahoo.com>
2018-11-25 13:55:40 +0200

    Added repositories

---
+++ src/main/java/com/backbase/kalah/persistence/repository/GameRepository.java
@@ -0,0 +1,9 @@
+package com.backbase.kalah.persistence.repository;
+
+import com.backbase.kalah.persistence.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
+}
---
+++ src/main/java/com/backbase/kalah/persistence/repository/PitRepository.java
@@ -0,0 +1,9 @@
+package com.backbase.kalah.persistence.repository;
+
+import com.backbase.kalah.persistence.model.Pit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PitRepository extends JpaRepository<Pit, Long> {
+}
