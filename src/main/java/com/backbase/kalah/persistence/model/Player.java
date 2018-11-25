a354caa57a3c3a1f684dea8fcdeb2e327bd2a1ed
Shingirai Chanakira <sechanakira@yahoo.com>
2018-11-25 14:08:11 +0200

    Changed to ArrayList for faster searching

--- src/main/java/com/backbase/kalah/persistence/model/Player.java
+++ src/main/java/com/backbase/kalah/persistence/model/Player.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -19,7 +19,7 @@ public class Player implements Serializable {
     @ElementCollection
     @Column(name = "pits")
     @CollectionTable(name="player_pits", joinColumns=@JoinColumn(name="player_pit_id"))
-    private List<Pit> pits = new LinkedList<>();
+    private List<Pit> pits = new ArrayList<>();
     @Embedded
     private House house;
 
