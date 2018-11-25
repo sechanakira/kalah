dbd616596a754138b30207472172496ccd83d71a
Shingirai Chanakira <sechanakira@yahoo.com>
2018-11-25 16:45:04 +0200

    Added find game and make move controllers

--- src/main/java/com/backbase/kalah/api/GameController.java
+++ src/main/java/com/backbase/kalah/api/GameController.java
@@ -1,11 +1,13 @@
 package com.backbase.kalah.api;
 
 import com.backbase.kalah.dto.GameDTO;
+import com.backbase.kalah.dto.GameStatusDTO;
 import com.backbase.kalah.persistence.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/games")
@@ -17,5 +19,15 @@ public class GameController {
     @PostMapping
     public GameDTO createGame(){
         return gameService.createGame();
+    }
+
+    @GetMapping("/{id}")
+    public GameDTO findGame(@PathVariable("id") @Valid @NotNull Long id){
+        return gameService.findGame(id);
+    }
+
+    @PutMapping("/{gameId}/pits/{pitId}")
+    public GameStatusDTO makeMove(@PathVariable("gameId") @Valid @NotNull Long gameId, @PathVariable("pitId") @Valid @NotNull Long pitId){
+        return gameService.makeMove(gameId,pitId);
     }
 }
