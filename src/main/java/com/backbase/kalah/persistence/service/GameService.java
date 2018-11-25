95611d01461378cb25457e74b941ba9a681aca04
Shingirai Chanakira <sechanakira@yahoo.com>
2018-11-25 17:13:30 +0200

    Added makeMove and findGame methods

--- src/main/java/com/backbase/kalah/persistence/service/GameService.java
+++ src/main/java/com/backbase/kalah/persistence/service/GameService.java
@@ -1,10 +1,14 @@
 package com.backbase.kalah.persistence.service;
 
 import com.backbase.kalah.dto.GameDTO;
+import com.backbase.kalah.dto.GameStatusDTO;
 
-@FunctionalInterface
 public interface GameService {
 
     GameDTO createGame();
+
+    GameDTO findGame(Long id);
+
+    GameStatusDTO makeMove(Long gameId,Long pitId);
 
 }
