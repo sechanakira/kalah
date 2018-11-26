8a4f3fdb3e294afc3d5acd8c14b88d04c02c8213
Shingirai Chanakira <sechanakira@yahoo.com>
2018-11-26 06:04:50 +0200

    Implemented GameService

--- src/main/java/com/backbase/kalah/persistence/service/GameServiceImpl.java
+++ src/main/java/com/backbase/kalah/persistence/service/GameServiceImpl.java
@@ -1,8 +1,9 @@
 package com.backbase.kalah.persistence.service;
 
 import com.backbase.kalah.dto.GameDTO;
+import com.backbase.kalah.dto.GameStatusDTO;
+import com.backbase.kalah.exception.*;
 import com.backbase.kalah.persistence.model.Game;
-import com.backbase.kalah.persistence.model.House;
 import com.backbase.kalah.persistence.model.Pit;
 import com.backbase.kalah.persistence.model.Player;
 import com.backbase.kalah.persistence.repository.GameRepository;
@@ -12,10 +13,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.stream.IntStream;
+import java.util.*;
+import java.util.stream.LongStream;
 
 import static com.backbase.kalah.constant.ApplicationConstants.GAME_RESOURCE_BASE_URL;
 
@@ -44,20 +43,121 @@ public class GameServiceImpl implements GameService {
         return dto;
     }
 
+    @Override
+    public GameDTO findGame(Long id) {
+        GameDTO gameDTO;
+        Optional<Game> found = gameRepository.findById(id);
+        if(found.isPresent()){
+            gameDTO = mapper.map(found.get(),GameDTO.class);
+            gameDTO.setUri(createResourceUri(found.get().getId()));
+        }else {
+            throw new GameNotFoundException();
+        }
+        return gameDTO;
+    }
+
+    @Override
+    public GameStatusDTO makeMove(Long gameId, Long pitId) {
+        if(gameId == null){
+            throw new MissingVariableException("Required param gameId is missing from request");
+        }
+        Optional<Game> fetched = gameRepository.findById(gameId);
+        if(pitId == null){
+            throw new MissingVariableException("Required param pitId is missing from request");
+        }
+        if(pitId < 0 || pitId > 14){
+            throw new InvalidPitIdException();
+        }
+        if(fetched.isPresent()){
+            Game game = fetched.get();
+            if(game.isGameWon()){
+                throw new GameEndedException("Game was won by " + game.getWinner() + " and is now ended");
+            }
+            return move(game,pitId);
+        }else{
+            throw new GameNotFoundException();
+        }
+    }
+
+    private GameStatusDTO move(Game game, Long pitId){
+        boolean isPlayer1 = false;
+        if(pitId < 7){
+            isPlayer1 = true;
+        }
+        if(!isPlayer1 && game.getPlayer1PlaysNext() != null && game.getPlayer1PlaysNext() == true){
+            throw new NotYourTurnException();
+        }
+        if(isPlayer1 && game.getPlayer2PlaysNext() != null && game.getPlayer2PlaysNext() == true){
+            throw new NotYourTurnException();
+        }
+        List<Pit> pits = new ArrayList<>();
+        for(Player player : game.getPlayers()){
+            pits.addAll(player.getPits());
+        }
+        pits.sort(Comparator.comparing(Pit::getPitId));
+        Pit start = pitRepository.findByPitId(pitId);
+        int startStones = start.getStones();
+        start.setStones(0);
+        Long lastPitId = 0L;
+        Long currentPitId = start.getPitId() + 1;
+        for(int j = 1; j <= startStones; j++){
+            if(currentPitId > 14){
+                currentPitId = 1L;
+            }
+            if(currentPitId == 14 && isPlayer1){
+                continue;
+            }
+            if(currentPitId == 7 && !isPlayer1){
+                continue;
+            }
+            Pit pit = pitRepository.findByPitId(currentPitId);
+            pit.setStones(pit.getStones() + 1);
+            lastPitId = currentPitId;
+            currentPitId++;
+        }
+        if(isPlayer1 && lastPitId == 7){
+            game.setPlayer1PlaysNext(true);
+        }
+        if(!isPlayer1 && lastPitId == 14){
+            game.setPlayer2PlaysNext(true);
+        }
+        GameStatusDTO dto = new GameStatusDTO();
+        dto.setId(game.getId());
+        dto.setUrl(createResourceUri(game.getId()));
+        dto.setStatus(createStatusMap(game));
+        return dto;
+    }
+
+    private Map<String,String> createStatusMap(Game game){
+        Map<String, String> map = new HashMap<>();
+        Player player1 = null;
+        Player player2 = null;
+        for(Player player : game.getPlayers()){
+            if(player.getPlayerId() == 1L){
+                player1 = player;
+            }
+            if(player.getPlayerId() == 2L){
+                player2 = player;
+            }
+        }
+        if(player1 == null || player2 == null){
+            throw new GameDataCorruptedException();
+        }
+        player1.getPits().forEach(pit->map.put(pit.getPitId().toString(),pit.getStones().toString()));
+        player2.getPits().forEach(pit->map.put(pit.getPitId().toString(),pit.getStones().toString()));
+        return map;
+    }
+
     private List<Player> createPlayers(){
         Player player1 = new Player();
-        House player1House = new House();
-        player1.setHouse(player1House);
         player1.setName("Player1");
         player1.setPlayerId(1L);
-        player1.setPits(createPlayerPits());
+        player1.setPits(createPlayerPits(true));
 
         Player player2 = new Player();
-        House player2House = new House();
-        player2.setHouse(player2House);
         player2.setName("Player2");
         player2.setPlayerId(2L);
-        player2.setPits(createPlayerPits());
+        player2.setPits(createPlayerPits(false));
 
         playerRepository.saveAll(Arrays.asList(player1,player2));
 
@@ -70,9 +170,23 @@ public class GameServiceImpl implements GameService {
         return sb.toString();
     }
 
-    private List<Pit> createPlayerPits(){
+    private List<Pit> createPlayerPits(boolean player1){
+        int startFrom;
+        int endAt;
+        if(player1){
+            startFrom = 1;
+            endAt = 7;
+        }else{
+            startFrom = 8;
+            endAt = 14;
+        }
         List<Pit> pits = new LinkedList<>();
-        IntStream.rangeClosed(1,6).forEach(i->pits.add(new Pit()));
+        LongStream.rangeClosed(startFrom,endAt).forEach(i->{
+            Pit pit = new Pit();
+            pit.setPitId(i);
+            pit.setStones(6);
+            pits.add(pit);
+        });
         pitRepository.saveAll(pits);
         return pits;
     }
